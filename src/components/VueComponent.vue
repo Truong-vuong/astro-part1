4ed6a20d3813fbc01fdcf728b571dac8cdc78ca3
Vuong Truong <vuong.truong@mgisolutions.com>
2023-08-04 19:03:52 +0700

    first commit

---
+++ src/components/VueComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['title'])
+
+console.log(props)
+</script>
+<template>
+    <h1>Vue component {{ title }}</h1>
+</template>
---
+++ src/components/VueComponent2.vue
@@ -0,0 +1,6 @@
+<script setup>
+console.log(123)
+</script>
+<template>
+    <h1>Vue component 2</h1>
+</template>
